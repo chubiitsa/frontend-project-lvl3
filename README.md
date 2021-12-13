672325f3cc1fff3045433f9bf672fe3c1c63dcb8
Anna <anna.chubiy@gmail.com>
2021-12-13 22:09:43 +0300

    Change code after first check

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-### Hexlet tests and linter status:
+# RSS aggregator
+
 [![Actions Status](https://github.com/chubiitsa/frontend-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/chubiitsa/frontend-project-lvl3/actions)
 [![eslint](https://github.com/chubiitsa/frontend-project-lvl3/workflows/eslint/badge.svg)](https://github.com/chubiitsa/frontend-project-lvl3/actions)
-
 [![Maintainability](https://api.codeclimate.com/v1/badges/95746f7a9f498a424e65/maintainability)](https://codeclimate.com/github/chubiitsa/frontend-project-lvl3/maintainability)
 
-[Мой проект на Vercel](https://frontend-project-mu.vercel.app/)
+Service for aggregate rss feeds with an automatic update each 5 seconds.
+
+## Usage 
+
+Copy & paste the following link to check how it works:
+[Lorem ipsum RSS](http://lorem-rss.herokuapp.com/feed?unit=second) (new post arrives each second).
+
+[Try it](https://frontend-project-mu.vercel.app/)
+
+## Main feature
+
+Subscribing to changes in the State object made it possible to implement the MVC pattern & split code into modules.
+
+## Build with
+
+* Webpack
+* Bootstrap
+* i18next for texts
+* yup for validation
+* testing-library for tests
+* on-change to watch changes
--- src/view.js
+++ src/view.js
@@ -1,37 +1,31 @@
 import onChange from 'on-change';
 
 const renderError = (errorMessage, elements) => {
-  const previousMessage = elements.form.nextSibling;
-  if (previousMessage) {
-    previousMessage.remove();
-  }
-  const errorContainer = document.createElement('p');
-  errorContainer.classList.add('text-danger');
-  errorContainer.textContent = errorMessage;
-  elements.form.after(errorContainer);
+  elements.messageContainer.textContent = '';
+  const errorEl = document.createElement('p');
+  errorEl.classList.add('text-danger');
+  errorEl.textContent = errorMessage;
+  elements.messageContainer.append(errorEl);
   elements.input.classList.add('is-invalid', 'form-control');
   elements.input.select();
 };
 
 const renderProgressMessage = (state, elements, translator) => {
-  const previousMessage = elements.form.nextSibling;
-  if (previousMessage) {
-    previousMessage.remove();
-  }
-  const messageContainer = document.createElement('p');
+  elements.messageContainer.textContent = '';
+  const messageEl = document.createElement('p');
   switch (state.loadingProcess.status) {
     case 'loading':
-      messageContainer.textContent = translator('messages.progress');
-      messageContainer.classList.add('text-info');
-      elements.form.after(messageContainer);
+      messageEl.textContent = translator('messages.progress');
+      messageEl.classList.add('text-info');
+      elements.messageContainer.append(messageEl);
       break;
     case 'idle':
-      messageContainer.textContent = translator('messages.success');
-      messageContainer.classList.add('text-success');
-      elements.form.after(messageContainer);
+      messageEl.textContent = translator('messages.success');
+      messageEl.classList.add('text-success');
+      elements.messageContainer.append(messageEl);
       break;
     case 'failed':
-      elements.input.removeAttribute('readonly');
+      elements.input.readOnly = false;
       renderError(state.loadingProcess.error, elements);
       break;
 
@@ -42,7 +36,7 @@ const renderProgressMessage = (state, elements, translator) => {
 
 const renderItem = (item, elements, state, translator) => {
   const titleElement = document.createElement('a');
-  titleElement.setAttribute('href', item.link);
+  titleElement.href = item.link;
   titleElement.setAttribute('target', '_blank');
   titleElement.textContent = item.title;
   titleElement.setAttribute('data-id', item.postId);
@@ -85,23 +79,24 @@ const renderFeeds = (feeds, elements) => {
 const renderForm = (state, elements) => {
   switch (state.form.status) {
     case 'filling':
-      elements.submitBtn.removeAttribute('disabled');
-      elements.input.removeAttribute('readonly');
       elements.input.classList.remove('is-invalid');
+      elements.submitBtn.disabled = false;
+      elements.input.readOnly = false;
       elements.input.value = '';
       break;
 
     case 'read-only':
-      elements.submitBtn.setAttribute('disabled', true);
       elements.input.classList.remove('is-invalid');
-      elements.input.setAttribute('readonly', true);
+      elements.submitBtn.setAttribute('disabled', true);
+      elements.input.readOnly = true;
       break;
 
     case 'failed':
       elements.input.classList.add('is-invalid');
-      elements.submitBtn.removeAttribute('disabled');
-      elements.input.removeAttribute('disabled');
+      elements.submitBtn.disabled = false;
+      elements.input.readOnly = false;
       elements.input.focus();
+      renderError(state.form.error, elements);
       break;
 
     default:
@@ -110,28 +105,28 @@ const renderForm = (state, elements) => {
 };
 
 const renderModal = (state, elements) => {
-  const postData = state.posts.filter((post) => post.postId === state.modal.openedPost);
-  const [{ title, description, link }] = postData;
+  const postData = state.posts.find((post) => post.postId === state.modal.openedPost);
+  const { title, description, link } = postData;
   const modalTitle = elements.modal.querySelector('.modal-title');
   const modalDescription = elements.modal.querySelector('.modal-description');
   const linkButton = elements.modal.querySelector('.modal-full-post-link');
   modalTitle.textContent = title;
   modalDescription.textContent = description;
-  linkButton.setAttribute('href', link);
+  linkButton.href = link;
 };
 
 const renderPage = (state, elements, translator) => {
   elements.header.textContent = translator('header');
   elements.description.textContent = translator('description');
-  elements.inputPlaceholder.setAttribute('placeholder', translator('input-placeholder'));
+  elements.input.placeholder = translator('placeholder');
   elements.addButton.textContent = translator('buttons.add');
   elements.exampleElement.textContent = translator('example');
-  elements.feedsTitle.textContent = translator('feeds-title');
-  elements.feedsDescription.textContent = translator('feeds-description');
-  elements.postsTitle.textContent = translator('posts-title');
-  elements.postsDescription.textContent = translator('posts-description');
-  elements.modalLink.textContent = translator('modal-full-post-link');
-  elements.modalClose.textContent = translator('buttons.modal-close-btn');
+  elements.feedsTitle.textContent = translator('feedsTitle');
+  elements.feedsDescription.textContent = translator('feedsDescription');
+  elements.postsTitle.textContent = translator('postsTitle');
+  elements.postsDescription.textContent = translator('postsDescription');
+  elements.modalLink.textContent = translator('modalLink');
+  elements.modalClose.textContent = translator('buttons.modalCloseBtn');
 };
 
 const initView = (state, elements, translator) => {
@@ -139,7 +134,6 @@ const initView = (state, elements, translator) => {
 
   const mapping = {
     'form.status': () => renderForm(state, elements, translator),
-    'form.error': () => renderError(state.form.error, elements),
     'loadingProcess.status': () => renderProgressMessage(state, elements, translator),
     'modal.openedPost': () => renderModal(state, elements),
     'ui.seenPosts': () => renderPosts(state, elements, translator),
